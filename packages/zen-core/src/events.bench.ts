70c567928c9a6411ee16c652f34228a990bf967c
shtse8 <shtse8@gmail.com>
2025-04-17 04:50:06 +0100

    fix(bench): update imports and remove obsolete benchmark files

--- packages/zen-core/src/events.bench.ts
+++ packages/zen-core/src/events.bench.ts
@@ -1,9 +1,9 @@
 
 import { bench, describe } from 'vitest'
-import { createAtom, set as setAtom, subscribe as subscribeAtom } from './atom'
-import { createComputed, subscribe as subscribeComputed } from './computed'
-import { createMap, setKey as setMapKey, subscribe as subscribeMap } from './map'
-import { createDeepMap, setPath as setDeepMapPath, subscribe as subscribeDeepMap } from './deepMap'
+import { atom, set as setAtom, subscribe as subscribeAtom } from './atom' // Use 'atom', 'subscribe'
+import { computed } from './computed' // Use 'computed', remove subscribe import
+// import { createMap, setKey as setMapKey, subscribe as subscribeMap } from './map' // Removed map import
+import { deepMap, setPath as setDeepMapPath, subscribe as subscribeDeepMap } from './deepMap' // Use 'deepMap'
 import { atom as nanoAtom, computed as nanoComputed, map as nanoMap } from 'nanostores'
 import { onStart, onStop, onSet, onNotify, onMount } from './events' // Use default names
 import { onStart as nanoOnStart, onStop as nanoOnStop } from 'nanostores'
@@ -11,10 +11,10 @@ import { onStart as nanoOnStart, onStop as nanoOnStop } from 'nanostores'
 
 describe('onStart/onStop Overhead (Atom)', () => {
   bench('zen', () => {
-    const $a = createAtom(0)
-    const unsubStart = onStart($a, () => {})
-    const unsubStop = onStop($a, () => {})
-    const unsub = subscribeAtom($a, () => {})
+    const $a = atom(0) // Use atom factory
+    const unsubStart = onStart($a as any, () => {}) // Add type assertion
+    const unsubStop = onStop($a as any, () => {})   // Add type assertion
+    const unsub = subscribeAtom($a as any, () => {}) // Add type assertion
     unsub()
     unsubStart()
     unsubStop()
@@ -33,11 +33,12 @@ describe('onStart/onStop Overhead (Atom)', () => {
 
 describe('onStart/onStop Overhead (Computed)', () => {
    bench('zen', () => {
-    const $a = createAtom(0)
-    const $c = createComputed([$a], a => a)
-    const unsubStart = onStart($c, () => {})
-    const unsubStop = onStop($c, () => {})
-    const unsub = subscribeComputed($c, () => {})
+    const $a = atom(0) // Use atom factory
+    // Adjust computed callback signature and ensure input atom has assertion
+    const $c = computed([$a as any], (a: unknown) => a as number)
+    const unsubStart = onStart($c as any, () => {}) // Add type assertion
+    const unsubStop = onStop($c as any, () => {})   // Add type assertion
+    const unsub = subscribeAtom($c as any, () => {}) // Add type assertion
     unsub()
     unsubStart()
     unsubStop()
@@ -55,35 +56,35 @@ describe('onStart/onStop Overhead (Computed)', () => {
   })
 })
 
-describe('onStart/onStop Overhead (Map)', () => {
-    bench('zen', () => {
-        const $m = createMap({})
-        const unsubStart = onStart($m, () => {})
-        const unsubStop = onStop($m, () => {})
-        const unsub = subscribeMap($m, () => {})
-        unsub()
-        unsubStart()
-        unsubStop()
-    })
-
-    bench('nanostores', () => {
-        const $m = nanoMap({})
-        const unsubStart = nanoOnStart($m, () => {})
-        const unsubStop = nanoOnStop($m, () => {})
-        const unsub = $m.subscribe(() => {})
-        unsub()
-        unsubStart()
-        unsubStop()
-    })
-})
+// describe('onStart/onStop Overhead (Map)', () => { // Removed Map tests
+//     bench('zen', () => {
+//         const $m = createMap({})
+//         const unsubStart = onStart($m, () => {})
+//         const unsubStop = onStop($m, () => {})
+//         const unsub = subscribeMap($m, () => {})
+//         unsub()
+//         unsubStart()
+//         unsubStop()
+//     })
+//
+//     bench('nanostores', () => {
+//         const $m = nanoMap({})
+//         const unsubStart = nanoOnStart($m, () => {})
+//         const unsubStop = nanoOnStop($m, () => {})
+//         const unsub = $m.subscribe(() => {})
+//         unsub()
+//         unsubStart()
+//         unsubStop()
+//     })
+// })
 
 // Nanostores doesn't have deepMap equivalent for direct comparison
 describe('onStart/onStop Overhead (DeepMap - Zen only)', () => {
    bench('zen', () => {
-    const $dm = createDeepMap({})
-    const unsubStart = onStart($dm, () => {})
-    const unsubStop = onStop($dm, () => {})
-    const unsub = subscribeDeepMap($dm, () => {})
+    const $dm = deepMap({}) // Use deepMap factory
+    const unsubStart = onStart($dm as any, () => {}) // Add type assertion
+    const unsubStop = onStop($dm as any, () => {})   // Add type assertion
+    const unsub = subscribeDeepMap($dm as any, () => {}) // Add type assertion
     unsub()
     unsubStart()
     unsubStop()
@@ -93,46 +94,47 @@ describe('onStart/onStop Overhead (DeepMap - Zen only)', () => {
 // --- onSet / onNotify / onMount (Zen only) ---
 describe('Zen Specific Event Overheads', () => {
   bench('zen onSet overhead (atom)', () => {
-    const $a = createAtom(0)
-    const unsubSet = onSet($a, () => {})
+    const $a = atom(0) // Use atom factory
+    const unsubSet = onSet($a as any, () => {}) // Add type assertion
     setAtom($a, 1)
     unsubSet()
   })
 
-  bench('zen onSet overhead (map)', () => {
-    const $m = createMap<{ a?: number }>({})
-    const unsubSet = onSet($m, () => {})
-    setMapKey($m, 'a', 1)
-    unsubSet()
-  })
+  // bench('zen onSet overhead (map)', () => { // Removed Map test
+  //   const $m = createMap<{ a?: number }>({})
+  //   const unsubSet = onSet($m, () => {})
+  //   setMapKey($m, 'a', 1)
+  //   unsubSet()
+  // })
 
   bench('zen onSet overhead (deepMap)', () => {
-    const $dm = createDeepMap<{ a?: number }>({})
-    const unsubSet = onSet($dm, () => {})
+    const $dm = deepMap<{ a?: number }>({}) // Use deepMap factory
+    const unsubSet = onSet($dm as any, () => {}) // Add type assertion
     setDeepMapPath($dm, 'a', 1)
     unsubSet()
   })
 
   bench('zen onNotify overhead (atom)', () => {
-    const $a = createAtom(0)
-    const unsubNotify = onNotify($a, () => {})
-    subscribeAtom($a, () => {}) // Need a subscriber to trigger notify
+    const $a = atom(0) // Use atom factory
+    const unsubNotify = onNotify($a as any, () => {}) // Add type assertion
+    subscribeAtom($a as any, () => {}) // Need a subscriber to trigger notify, add type assertion
     setAtom($a, 1)
     unsubNotify()
   })
 
    bench('zen onNotify overhead (computed)', () => {
-    const $a = createAtom(0)
-    const $c = createComputed([$a], a => a)
-    const unsubNotify = onNotify($c, () => {})
-    subscribeComputed($c, () => {}) // Need a subscriber to trigger notify
+    const $a = atom(0) // Use atom factory
+    // Re-apply fixes: add assertion to input atom, adjust callback, add assertion to onNotify/subscribeAtom
+    const $c = computed([$a as any], (a: unknown) => a as number)
+    const unsubNotify = onNotify($c as any, () => {}) // Add type assertion
+    subscribeAtom($c as any, () => {}) // Add type assertion
     setAtom($a, 1) // Trigger computed update
     unsubNotify()
   })
 
   bench('zen onMount overhead (atom)', () => {
-    const $a = createAtom(0) // Use createAtom
-    const unsubMount = onMount($a, () => {}) // Use onMount, Called immediately
+    const $a = atom(0) // Use atom factory
+    const unsubMount = onMount($a as any, () => {}) // Use onMount, Called immediately, add type assertion
     unsubMount()
   })
 })
